56511c3404812410301691eba9ab8dd070defc6e
apinanlamt <apple8511id@gmail.com>
2025-09-19 21:42:33 +0700

    self-practice-W06

---
+++ week06/practice06.js
@@ -0,0 +1,72 @@
+// Lesson 1: Arrays, Objects, and Functions
+
+// 1. Create an empty array to hold the quotes
+const quotes = []
+
+/*
+  2. Function: addQuote
+  - Accepts a quote object with id, content, and author
+  - Adds it to the quotes array
+*/
+function addQuote(quote) {
+  quotes.push(quote)
+}
+
+/*
+  3. Function: deleteQuote
+  - Accepts an id
+  - Removes the quote with that id from the array
+*/
+function deleteQuote(id) {
+  const index = quotes.findIndex(quote => quote.id === id)
+  if (index !== -1){
+    quotes.splice(index, 1)
+  }
+}
+
+/*
+  4. Function: updateQuote
+  - Accepts an id and an object with new content and/or author
+  - Updates the quote with the given id
+*/
+function updateQuote(id, updatedQuote) {
+  const quote = quotes.find(quote => quote.id === id)
+  if (quote) {
+    if (updatedQuote.content) {
+      quote.content = updatedQuote.content
+    }
+    if (updatedQuote.author) {
+      quote.author = updatedQuote.author
+    }
+  }
+}
+
+/*
+  5. Function: getAllQuotes
+  - Returns all quotes in the array
+*/
+function getAllQuotes() {
+  return quotes
+}
+
+// 6. Test your functions below
+// Add 3 quotes
+addQuote({ id: 1, content: 'Stay hungry, stay foolish.', author: 'Steve Jobs' })
+addQuote({ id: 2, content: 'Do or do not. There is no try.', author: 'Yoda' })
+addQuote({
+  id: 3,
+  content: 'Simplicity is the ultimate sophistication.',
+  author: 'Leonardo da Vinci'
+})
+
+console.log(getAllQuotes());
+
+// Delete 1 quote
+deleteQuote(2)
+console.log(getAllQuotes());
+
+// Update 1 quote
+updateQuote(1, { content: 'Stay hungry. Stay foolish.', author: 'Jobs' })
+
+// Print all quotes
+console.log(getAllQuotes())
